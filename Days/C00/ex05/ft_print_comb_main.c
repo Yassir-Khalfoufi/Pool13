ee29153f910c2223d9f4335f4b8aca761b663c37
Yassir-Khalfoufi <yassircareer@gmail.com>
2026-07-20 15:48:32 +0100

    lmlih safi21

--- Days/C00/ex05/ft_print_comb_main.c
+++ Days/C00/ex05/ft_print_comb_main.c
@@ -4,19 +4,19 @@ void	ft_print_comb()
 	int	a;
 	int	b;
 	int	c;
-	a = '0';
-	while(a <= '7')
+	a = 48;
+	while(a <= 55)
 	{	
 		b = a + 1;
-		while(b <= '8')
+		while(b <= 56)
 		{
 			c = b + 1;
-			while(c <= '9')
+			while(c <= 57)
 			{
 				write(1, &a, 1);
 				write(1, &b, 1);
 				write(1, &c, 1);
-				if (a != '7')
+				if (a != 55)
 					write(1, ", ",2);
 				c++;
 			}
