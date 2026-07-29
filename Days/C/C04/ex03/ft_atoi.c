b0bb2bfd37a0a8f3d98fb22c0b27a6533f3b8733
Yassir-Khalfoufi <yassircareer@gmail.com>
2026-07-29 22:38:41 +0100

    rev

--- Days/C/C04/ex03/ft_atoi.c
+++ Days/C/C04/ex03/ft_atoi.c
@@ -8,14 +8,14 @@ int	ft_atoi(char *str)
 	result = 0;
 	if(!str)
 		return(0);
-	while(str[i] == ' ' || (str[i] >= 9 && str[i] >= 13))
+	while(str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
 	{
 		i++;
 	}
-	if(str[i] == '-' || str[i] == '+')
+	while(str[i] == '-' || str[i] == '+')
 	{
 		if(str[i] == '-')
-			sign = -1;
+			sign = -sign;
 		i++;
 	}
 	while(str[i] >= '0' && str[i] <= '9')
--- Days/C/C04/ex03/ft_atoi_main.c
+++ Days/C/C04/ex03/ft_atoi_main.c
@@ -14,7 +14,7 @@ int	ft_atoi(char *str)
 	while(str[i] == '-' || str[i] == '+')
 	{
 		if(str[i] == '-')
-			sign = -1;
+			sign = -sign;
 		i++;
 	}
 	while(str[i] >= '0' && str[i] <= '9')
