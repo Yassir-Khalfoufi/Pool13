2b3aff799271c61b28d0534d05cadae8317cd4f8
Yassir-Khalfoufi <yassircareer@gmail.com>
2026-07-31 21:42:58 +0100

    sqrt

---
+++ Days/C/C05/ex05/ft_sqrt.c
@@ -0,0 +1,18 @@
+int ft_sqrt(int nb)
+{
+    if (nb < 0)
+        return 0;
+    if (nb == 0)
+        return 0;
+    if (nb == 1)
+        return 1;
+    int    i;
+    i = 1;
+    while(i * i < nb)
+    {
+        i++;
+        if (i * i == nb)
+            return (i);
+    }
+    return 0;
+}
