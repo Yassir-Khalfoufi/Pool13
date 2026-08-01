b0684b240b6cb0e3fe732046e9bd74a44c4beb7e
Yassir-Khalfoufi <yassircareer@gmail.com>
2026-08-01 19:57:15 +0100

    revp

---
+++ Days/C/C06/ex02/ft_rev_params.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+int    main(int    argc, char   **argv)
+{
+    int    i;
+    int    j;
+    i = argc - 1;
+    if (argc < 2)
+        return 0;
+    while (i >= 1)
+    {
+        j = 0;
+        while (argv[i][j] != '\0')
+        {
+            write(1, &argv[i][j], 1);
+            j++;
+        }
+        write(1, "\n", 1);
+        i--;
+    }
+}
