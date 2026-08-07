4daaed0c42c1a39f5023f389c3b325cee29197b2
Yassir-Khalfoufi <yassircareer@gmail.com>
2026-08-07 16:38:24 +0100

    isa

--- Days/C/C02/ex01/ft_strncpy.c
+++ Days/C/C02/ex01/ft_strncpy.c
@@ -1,11 +1,9 @@
-#include<stdio.h>
-#include<string.h>
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int	i;
 	i = 0;
 	if(!src)
-		return(NULL);
+		return("NULL");
 	while(src[i] != '\0' && n > i)
 	{
 		dest[i] = src[i];
--- Days/C/C02/ex02/ft_str_is_alpha.c
+++ Days/C/C02/ex02/ft_str_is_alpha.c
@@ -2,6 +2,8 @@ int	ft_str_is_alpha(char *str)
 {
 	int	i;
 	i = 0;
+	if (!str)
+		return 0;
 	while(str[i] != '\0')
 	{
 		if((str[i] < 'a' || str[i] > 'z') && (str[i] < 'A' || str[i] > 'Z'))
