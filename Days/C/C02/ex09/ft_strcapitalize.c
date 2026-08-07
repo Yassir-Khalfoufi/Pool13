178c8899ebd2850ba5cd99894aeab4643d36612b
Yassir-Khalfoufi <yassircareer@gmail.com>
2026-08-07 17:22:32 +0100

    ja

--- Days/C/C02/ex09/ft_strcapitalize.c
+++ Days/C/C02/ex09/ft_strcapitalize.c
@@ -6,15 +6,15 @@ char	*ft_strcapitalize(char *str)
 	prev = 1;
 	while(str[i] != '\0')
 	{
-	if(str[i] >= 'a' && str[i] <= 'z' && prev == 1)
-		str[i] -= 32;
-	else if(str[i] >= 'A' && str[i] <= 'Z' && prev == 0)
-		str[i] += 32;
-	if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= '0' && str[i] <= '9'))
-		prev = 0;
-	else
-		prev = 1;
-	i++;
+		if(str[i] >= 'a' && str[i] <= 'z' && prev == 1)
+			str[i] -= 32;
+		else if(str[i] >= 'A' && str[i] <= 'Z' && prev == 0)
+			str[i] += 32;
+		if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= '0' && str[i] <= '9'))
+			prev = 0;
+		else
+			prev = 1;
+		i++;
 	}
 	return(str);
 }
